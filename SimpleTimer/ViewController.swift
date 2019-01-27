2b0193c3ccaf20613ddc29e06f3201f9e1630837
Adi Ricky K <it.adiricky@gmail.com>
2019-01-27 21:01:46 +0700

    Initial Commit

---
+++ SimpleTimer/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SimpleTimer
+//
+//  Created by Black Vanilla on 27/01/19.
+//  Copyright © 2019 Black Vanilla. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
